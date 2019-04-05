d46a0563a9119c15150ac218e519c0189b423bc9
luiss6 <44926678+luiss6@users.noreply.github.com>
2019-04-05 17:25:30 +0100

    added titles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import AnnieHall from './AnnieHall.jpg';
 import './App.css';
 import TVseries from "./TVseries";
+import Titles from "./Titles";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={AnnieHall} alt="Annie Hall" />
-        <TVseries />
+        <img src={AnnieHall} alt="Annie Hall" className="fave"/>
+        <Titles />
       </div>
       
 
---
+++ src/Titles.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Titles () {
+    return(
+        <div className="series">
+            <div className="titles">
+                <div className="flex1">
+                    <p>X-files</p><br></br>
+                    <p>Mr Robot</p><br></br>
+                    <p>Homeland</p><br></br>
+                </div>
+                <div className="flex2">
+                    <p>The office</p><br></br>
+                    <p>Seinfeld</p><br></br>
+                    <p>Magnum PI</p><br></br>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Titles;
