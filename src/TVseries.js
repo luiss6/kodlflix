8974568c7d5ab9a1bba8cbc5b77321e790b249a3
luiss6 <44926678+luiss6@users.noreply.github.com>
2019-04-09 19:13:23 +0100

    added titles overlay

--- src/TVseries.js
+++ src/TVseries.js
@@ -7,28 +7,39 @@ import img5 from "./Series/img5.jpg";
 import img6 from "./Series/img6.jpg";
 
 function TVseries() {
-    return(
+    return (
         <div className="series">
-            <div className="container">
-                <div>
-                    <img src={img1} className="item" alt="cover pic"></img>
-                </div> 
-                <div>  
-                    <img src={img2} className="item" alt="cover pic"></img>
+            <div className="box">
+                <div className="container">
+                    <img src={img1} className="item" alt="cover pic" />
+                    <div className="title overlay">X-files</div>
+
+                </div >
+                <div className="container">
+                    <img src={img2} className="item" alt="cover pic" />
+                    <div className="title overlay">Homeland</div>
+
                 </div>
-                <div> 
-                    <img src={img3} className="item" alt="cover pic"></img>
-                </div>   
-            </div>           
-            <div className="container">
-                <div> 
-                    <img src={img4} className="item" alt="cover pic"></img>
+                <div className="container">
+                    <img src={img3} className="item" alt="cover pic" />
+                    <div className="title overlay">Mr Robot</div>
+
                 </div>
-                <div>    
-                    <img src={img5} className="item" alt="cover pic"></img>
+            </div>
+            <div className="box">
+                <div className="container">
+                    <img src={img4} className="item" alt="cover pic" />
+                    <div className="title overlay">The office</div>
+
                 </div>
-                <div> 
-                    <img src={img6} className="item" alt="cover pic"></img>
+                <div className="container">
+                    <img src={img5} className="item" alt="cover pic" />
+                    <div className="title overlay">Seinfeld</div>
+
+                </div>
+                <div className="container">
+                    <img src={img6} className="item" alt="cover pic" />
+                    <div className="title overlay">Sherlock</div>
                 </div>
             </div>
         </div>
